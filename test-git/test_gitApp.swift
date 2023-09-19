2a66340dbfe77b288facd85593cf4c89d6561555
Will Parrish <willptci@gmail.com>
2023-09-19 09:54:05 -0400

    Initial Commit

---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Will Parrish on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
